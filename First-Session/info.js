12608d7fd4cf0e4eab00cf8072dc1a2d1aecb498
mostafa-dci <mostafa.othman@digitalcareerinstitute.org>
2019-08-29 09:26:20 +0200

    add files

---
+++ First-Session/info.js
@@ -0,0 +1,21 @@
+var myName = "Mostafa";// define a string variable
+var myAge = 32;// define a number variable
+var myCity = "Hamburg"; // define a string variable
+
+var info = "Hey I am " + myName + ". and i live in " + myCity + " and I am " + myAge + " years Old. ";
+// info is a string 
+console.log(info);
+var x = 6-5
+
+var firstNumber= 5;
+var seconfNumber= 10;
+var thirdNumber = 12;
+// The avrage is (5+10+12)/3
+var avrage = (firstNumber + seconfNumber + thirdNumber)/3
+console.log(avrage);
+var width = 10;
+var height = 5;
+var space =541;
+space=5*5
+console.log(space)
+
---
+++ First-Session/script.js
@@ -0,0 +1,38 @@
+//alert("Hey am an external javascript file called here");
+console.log(55.23);// Javascript knows Numbers
+
+// Javascript Knows Math Operations:
+// + * / - .......EX
+console.log(5*6);// = 30
+console.log(9-5*2); // = -1 javascript also knows nigative values
+
+// javascript knows Strings, text EX:
+console.log("Hey am just a text or string");
+
+// Javascript can define Variables
+// to define a variable use  keyword "var"
+var x=5;
+console.log(x);
+// rules for variables:
+// no number before the variable name EX
+//var 1y=10; Wrooooooong!!!!!
+//var +y=10; Wroooong
+//var %y=10;
+var y1=10;// ok goot it
+var _name="Hey";// yes yo can
+// dont use the javascript Keywords
+//var class=10
+//var var=10
+//var const=10;
+//javascript sensetive to upper lower cases ex:
+var Myname="Mostafa";
+var myname="BMW";
+console.log(myname);// = BMW
+console.log(Myname);// = Mostafa
+
+// General Rule in Js: number +number = number
+// Number + string= string EX
+console.log(6+8); // => 14 number!
+console.log(5+"HI"); // => 5HI
+console.log("8"+"9");// => 89 a string value.
+
